b9cf628139f9c2e791e8e3dfa4d9abfc8fa6f8be
AmandaOdawa <amandaodawa1@gmail.com>
2025-04-04 18:51:12 +0300

    episodes resource

---
+++ resources/episodes.py
@@ -0,0 +1,16 @@
+from flask_restful import Resource
+from flask import jsonify
+from models import Episode
+
+class EpisodesListResource(Resource):
+    def get(self):
+        episodes = Episode.query.all()
+        return [episode.to_dict() for episode in episodes], 200
+    
+class EpisodeResource(Resource):
+    def get(self, id):
+        episode = Episode.query.get(id)
+        if episode:
+            return episode.to_dict_with_appearances(), 200
+        return {'error':'Episode nbot found'}, 404
+
